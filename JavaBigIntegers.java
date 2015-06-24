24e6d8887d39e9f31321f7df23eeef0d3ff069f1
ncmd <ncmd@users.noreply.github.com>
2015-06-24 16:14:10 -0700

    Update JavaBigIntegers.java

--- JavaBigIntegers.java
+++ JavaBigIntegers.java
@@ -1,11 +1,8 @@
-#	Problem Statement
-#	In this problem you have to add and multiply huge numbers! These numbers are so big that you can't contain them in any ordinary data types like long integer.
-#	Use the power of Java's BigInteger class and solve this problem.
-#	Input Format
-#	There will be two lines containing two numbers, a and b. The numbers are non-negative and can have maximum 200 digits.
-#	Output Format
-#	Output two lines. The first line should contain a+b, and the second line should contain a×b. Don't print any leading zeros.
-
+/**
+ * Challenge: https://www.hackerrank.com/challenges/java-biginteger
+ * 
+ * Made by Charles M. Chong
+ */
 import java.io.*;
 import java.util.*;
 import java.text.*;
